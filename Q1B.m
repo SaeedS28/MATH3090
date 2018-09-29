984a0ccd357e4eba1dacffa5bef4036b55939181
SaeedS28 <awesomesaad.saeed2@gmail.com>
2018-09-28 23:31:41 -0400

    added error checking

--- Q1B.m
+++ Q1B.m
@@ -1,5 +1,5 @@
 function [lowerBound, upperBound] = Q1B(a,b)
-% Bisection Method for y=-cosx-x.^3
+% Bisection Method for a function
 
 lowerBound=a;
 upperBound=b;
@@ -7,11 +7,14 @@ upperBound=b;
 %sprintf("Value of lowerbound %f",a)
 %sprintf("Value of upperbound %f",b)
 
-
+if (hardCodedFunction(lowerBound) * hardCodedFunction(upperBound) >0)
+    error("The initial guesses are incorrect: lower*upper<0 is the satisfying condition.");
+end
 
 end
 
 function y=hardCodedFunction(x)
+% The hardcoded function happened to be y=-cosx-x.^3
 
 y = (-cos(x)-x.^3);
 
